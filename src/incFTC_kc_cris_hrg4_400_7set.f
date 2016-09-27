af5e4fe69b7e915d6457930f49aa77193817d183
L. Larrabee Strow <strow@umbc.edu>
2016-09-27 10:12:27 -0400

    Benchmark for new sarta + database
    Simplified incFTC, too many comments are confusing.
    
    Database is now set to be:
      /asl/data/sarta_database/Data_CrIS_sep16
    which is presently (soon) a symbolic link to
    whatever directory Chris wants to use.
    
    The database is now in git using lfs.
    git clone git@github.com:strow/Data_CrIS_sep16.git

--- src/incFTC_kc_cris_hrg4_400_7set.f
+++ src/incFTC_kc_cris_hrg4_400_7set.f
@@ -445,56 +445,25 @@ C      ---------
        CHARACTER*90 FNCOFN ! non-LTE
 C
        PARAMETER(FNCOF1='/asl/data/sarta_database/Data_CrIS_sep16/Coef/set1.dat')
-c       PARAMETER(FNCOF1='../Data/set1_uncut.dat')
-c       PARAMETER(FNCOF1='../Data/dummy_set1.dat')
-C
        PARAMETER(FNCOF2='/asl/data/sarta_database/Data_CrIS_sep16/Coef/set2.dat')
-c       PARAMETER(FNCOF2='../Data/set2_uncut.dat')
-c       PARAMETER(FNCOF2='../Data/dummy_set2.dat')
-C
        PARAMETER(FNCOF3='/asl/data/sarta_database/Data_CrIS_sep16/Coef/set3.dat')
-c       PARAMETER(FNCOF3='../Data/set3_uncut.dat')
-C
        PARAMETER(FNCOF4='/asl/data/sarta_database/Data_CrIS_sep16/Coef/set4.dat')
-c       PARAMETER(FNCOF4='../Data/set4_uncut.dat')
-c       PARAMETER(FNCOF4='../Data/dummy_set4.dat')
-C
        PARAMETER(FNCOF5='/asl/data/sarta_database/Data_CrIS_sep16/Coef/set5.dat')
-c       PARAMETER(FNCOF5='../Data/set5_uncut.dat')
-c       PARAMETER(FNCOF5='../Data/dummy_set5.dat')
-C
        PARAMETER(FNCOF6='/asl/data/sarta_database/Data_CrIS_sep16/Coef/set6.dat')
-c       PARAMETER(FNCOF6='../Data/set6_uncut.dat')
-c       PARAMETER(FNCOF6='../Data/dummy_set6.dat')
-C
        PARAMETER(FNCOF7='/asl/data/sarta_database/Data_CrIS_sep16/Coef/set7.dat')
-c       PARAMETER(FNCOF7='../Data/set7_uncut.dat')
-c       PARAMETER(FNCOF7='../Data/dummy_set7.dat')
-C
-C       PARAMETER(FNCO2='/asl/data/sarta_database/Data_CrIS_sep16/Coef/co2.dat')
-c       PARAMETER(FNCO2 ='../Data/rawcoef_co2_fowp.dat')
-C       PARAMETER(FNCO2 ='/home/chepplew/gitLib/ftc_dev/run/400ppm/CrIS_h4_rawcoef_co2_5term_fowp_sun.dat')
-c       PARAMETER(FNCO2 ='../Data/dummy_co2.dat')
 C
        PARAMETER(FNOPTR='/asl/data/sarta_database/Data_CrIS_sep16/Coef/optran.dat')
-c       PARAMETER(FNOPTR='../Data/optran1_uncut.dat')
-c       PARAMETER(FNOPTR='../Data/optran2_uncut.dat')
-c       PARAMETER(FNOPTR='../Data/optran_uncut.dat')
-c       PARAMETER(FNOPTR='../Data/optran_uncut_new.dat')
-c       PARAMETER(FNOPTR='../Data/dummy_optran.dat')
-C
+C 
        PARAMETER(FNN2O='/asl/data/sarta_database/Data_CrIS_sep16/Coef/n2o.dat')
        PARAMETER(FNSO2='/asl/data/sarta_database/Data_CrIS_sep16/Coef/so2.dat')
        PARAMETER(FNHNO3='/asl/data/sarta_database/Data_CrIS_sep16/Coef/hno3.dat')
+C
        PARAMETER(FNTHER='/asl/data/sarta_database/Data_CrIS_sep16/Coef/therm.dat')
        PARAMETER(FNCOFN='/asl/data/sarta_database/Data_CrIS_sep16/Coef/nte_7term.dat')
 C
        PARAMETER(FNFX='/asl/data/sarta_database/Data_CrIS_sep16/Coef/fx.txt')
-C       PARAMETER(FNPREF='/asl/s1/chepplew/data/sarta_database/Data_CrIS_hrg4_may16/Coef/profref_truJul00')
-C       PARAMETER(FNPREF='/asl/data/sarta_database/Data_IASI_sep08/Coef/profref_trace385')
        PARAMETER(FNPREF='/asl/data/sarta_database/Data_CrIS_sep16/Coef/refprof_regr49_1100_400ppm')
        PARAMETER(FNSUN='/asl/data/sarta_database/Data_CrIS_sep16/Solar/sol.txt')
-C
 C
 CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
 C Tuning filename
